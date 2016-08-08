3c0285dfd2250048179b1a10154c68e49596a45d
Haridas <haridasnykiel@gmail.com>
2016-08-08 14:22:07 +0100

    Made some more changes to the functions.

--- app.js
+++ app.js
@@ -4,46 +4,41 @@ $(function() {
 
   $('button').click(function(event) {
     event.preventDefault();
-
+    runCalculator();
 
   });
 
   // Go and get the stuff from the page.
 
-  $(document).ready (function () {
-    var buttons = $('.numbers').val();
+  function runCalculator() {
+    var buttons = $('button').val();
     var operator = $('.operator').val();
     var clear = $('#clear').val();
 
 
-
-
+    // console.log(buttons);
 
     for (var i = 0; i < buttons.length; i++) {
 
-      if (buttons[i].value === "="){
-        $('buttons').click (displayResult(calResult));
+      if (buttons[i] === "="){
+        displayResult(calResult);
       } else {
-        $('buttons').click (addValue(buttons));
+        addValue(buttons);
       }
 
-      console.log(buttons);
+
+
     }
-
-
 
     var calResult = calculate(buttons, buttons, operator);
 
-
     // console.log(calResult);
 
-
-
-  });
+  }
 
 
   // Create a function that does the maths.
-  $(function calculate(num1, num2, op) {
+  function calculate(num1, num2, op) {
     // ifs and elses (switch?) using num1, num2 and operator
 
 
@@ -75,20 +70,20 @@ $(function() {
       }
       // console.log(result);
       return result;
-  });
+  }
 
-  $(function addValue(input) {
+  function addValue(input) {
     $("#result").html(input);
-  });
+  }
 
   // Create a function that displays the results.
-  $(function displayResult(result) {
+  function displayResult(result) {
     // put result on the page
     // console.log(result);
     $("#result").html(result);
 
 
     // console.log(result);
-  });
+  }
 
 });
